3c31c2c5b97224dd4b4d0454aa2692bb25496b5f
Hannzo01 <kenzamzouri24@gmail.com>
2024-05-28 08:48:39 +0100

    n

---
+++ C00/ex06/ft_print_comb2.c
@@ -0,0 +1,33 @@
+#include <unistd.h>
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_print_comb2(void)
+{
+	int i, j;
+	i = 0;
+	while (i <= 98)
+	{
+		j = i + 1;
+		while (j <= 99)
+		{
+			ft_putchar (48 + i / 10);
+			ft_putchar (48 + i % 10);
+			ft_putchar (' ');
+			ft_putchar (48 + j / 10);
+			ft_putchar (48 + j % 10);
+			if (i != 98)
+				write(1,", ",2);
+		j++;
+		}
+	i++;		
+	}
+}
+/*
+int main()
+{
+	ft_print_comb2();
+}
+*/
